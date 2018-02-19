09bda40e74987bb4b5491fc35bc36f974268a2d2
Michal Tichý <michal.tichy@riganti.cz>
2018-02-19 19:19:33 +0100

    CREATE FRAME

---
+++ parse/instructions/CreateFrameInstruction.php
@@ -0,0 +1,11 @@
+<?php
+
+class CreateFrameInstruction implements IInstruction
+{
+    public function __construct($instructionTextRepresentation){
+    }
+
+    public function ToXmlElement(){
+
+    }
+}
