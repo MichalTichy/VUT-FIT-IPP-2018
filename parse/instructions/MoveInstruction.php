950f068ce9bbbfdcfb02b379ddd2e61c88896f1d
Michal Tichý <michal.tichy@riganti.cz>
2018-02-19 19:18:04 +0100

    MOVE

--- parse/instructions/MoveInstruction.php
+++ parse/instructions/MoveInstruction.php
@@ -1,12 +1,22 @@
 <?php
 class MoveInstruction implements IInstruction
 {
-    public function __construct(array $instructionTextRepresentation){
+    protected $arg1;
+    protected $arg2;
+    public function __construct($instructionTextRepresentation){
         $parser=new InstructionParser();
         $parser->CheckIfInstructionHaveAllParameters($instructionTextRepresentation,2);
+        $arg1=$parser->ExtractArgument($instructionTextRepresentation,1);
+        $arg2=$parser->ExtractArgument($instructionTextRepresentation,2);
+
+        $varValidator=new VariableValidator();
+        $varValidator->Validate($arg1);
+
+        $symbValidator=new SymbolValidator();
+        $symbValidator->Validate($arg2);
     }
 
     public function ToXmlElement(){
-        
+
     }
 }
